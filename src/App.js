b929448dd9b1a3386bb7b893a51517aba7d97940
Mayuri <dahakemayuri753@gmail.com>
2025-02-08 14:53:30 +0530

    Resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import AddJob from "./Admin/AddJob";
 import AddInternship from "./Admin/AddInternship";
 import Dashboard from "./Admin/Dashboard";
 import "./App.css";
-
-//import InternshipCard from './Frontend/Internship/InternshipCard';
+import InternshipCard from './Frontend/Internship/InternshipCard';
 import LoginPage from "./Admin/LoginPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-//import InternshipApplicationForm from './Frontend/Internship/InternshipApplicationForm';
+import InternshipApplicationForm from './Frontend/Internship/InternshipApplicationForm';
 //import Intenrships from './Pages/Intenrships';
 //import ManageApplicationJob from './Admin/ManageApplicationJob';
 //import ManageApplicationIntenrship from './Admin/ManageApplicationInternship';
@@ -18,8 +17,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <Router>
+      <Router>     
         <Routes>
+          <Route path="/internshipcard" element={<InternshipCard />} />
+          <Route path="/apply" element={<InternshipApplicationForm />} />
           <Route path="/" element={<LoginPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/addInternship" element={<AddInternship />} />

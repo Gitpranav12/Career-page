b5df64777ec03068a92d7ab747106bcca1c5be5e
harish <harrych_1@yahoo.com>
2025-02-10 16:23:12 +0530

    conflict solve

--- src/App.js
+++ src/App.js
@@ -1,59 +1,33 @@
-//import Searchbar from './Common/Searchbar';
-//import CareerDropdown from './Common/CareerDropdown';
-//import ApplicationForm from './Common/ApplicationForm';
 import Footer from "./Pages/Footer";
 import AddJob from "./Admin/AddJob";
 import AddInternship from "./Admin/AddInternship";
 import Dashboard from "./Admin/Dashboard";
 import "./App.css";
-<<<<<<< HEAD
-import InternshipCard from './Frontend/Internship/InternshipCard';
-=======
-<<<<<<< HEAD
-//import Home from "./Pages/Home";
-//import Navbar from "./Components/navbar";
-=======
-import Navbar from "./Components/navbar";
-import AdminNavbar from "./Components/AdminNavbar";
->>>>>>> 98d92993b76fad1baeeaf7c243b2ab0279a25dd1
-//import InternshipCard from './Frontend/Internship/InternshipCard';
->>>>>>> 76e4f15ecaf456f54eabd4674487abe77e1200da
+
+//import ConditionalNavbar from "./Components/ConditionalNavbar";
+
 import LoginPage from "./Admin/LoginPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-//app.js
-import InternshipApplicationForm from './Frontend/Internship/InternshipApplicationForm';
-//import Intenrships from './Pages/Intenrships';
-//import ManageApplicationJob from './Admin/ManageApplicationJob';
-import ManageApplicationIntenrship from './Admin/ManageApplicationInternship';
+
+import InternshipApplicationForm from "./Frontend/Internship/InternshipApplicationForm";
+import InternshipCard from "./Frontend/Internship/InternshipCard";
+import ManageApplicationInternship from "./Admin/ManageApplicationInternship";
 
 function App() {
   return (
-    <>
-<<<<<<< HEAD
-      <Router>     
-=======
-      <Router>
-<<<<<<< HEAD
+    <Router>
      
-=======
-      <ConditionalNavbar />
->>>>>>> 98d92993b76fad1baeeaf7c243b2ab0279a25dd1
->>>>>>> 76e4f15ecaf456f54eabd4674487abe77e1200da
-        <Routes>
-
-          <Route path="/internshipcard" element={<InternshipCard />} />
-          <Route path="/apply" element={<InternshipApplicationForm />} />
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/addInternship" element={<AddInternship />} />
-          <Route path="/addjob" element={<AddJob />} />
-          <Route path="/manageapplicationinternship" element={<ManageApplicationIntenrship />} />
-          
-
-        </Routes>
-        <Footer />
-      </Router>
-    </>
+      <Routes>
+        <Route path="/internshipcard" element={<InternshipCard />} />
+        <Route path="/apply" element={<InternshipApplicationForm />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/add-internship" element={<AddInternship />} />
+        <Route path="/add-job" element={<AddJob />} />
+        <Route path="/manage-applications-internship" element={<ManageApplicationInternship />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
